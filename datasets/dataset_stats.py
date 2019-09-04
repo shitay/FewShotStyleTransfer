0499b2ad690e3cfcc10823ca4b7126245fa8cc93
itay <shamiritay@gmail.com>
2019-09-04 12:36:01 +0300

    move files as separate function

--- datasets/dataset_stats.py
+++ datasets/dataset_stats.py
@@ -29,29 +29,26 @@ def split_train_test(train_per=0.9):
 
 def minimum_images():
     df = pd.read_csv('train.csv')
-    # df = df[df.filename.str.startswith('4') | df.filename.str.startswith('3') |
-    #         df.filename.str.startswith('2') | df.filename.str.startswith('1')]
-
     artists = list(set(df['artist']))
     counts = {artist: len(df[df.artist == artist]) for artist in artists if len(df[df.artist == artist]) >= 100}
-    # print(counts.keys())
     print(len(counts))
-    print(df.columns)
-    # for i, row in df.iterrows():
-    #     _, filename, artist, title, style, genere, date = row
-    #     if artist in counts:
-    #         src_path = os.path.join('train', filename)
-    #         dst_path = os.path.join('train_2', filename)
-    #         if os.path.exists(src_path):
-    #             if not os.path.exists(dst_path):
-    #                 shutil.copy(src_path, dst_path)
-
     df_train = df[df['artist'].isin(list(counts.keys()))]
     df_train.to_csv('new_train.csv')
     print(len(df_train))
 
-    # counts = list(Counter(df['artist']).values())
-    # print((len(counts), np.mean(counts), np.std(counts), np.min(counts), np.max(counts)))
+
+def move_files(old_path, new_path):
+    df = pd.read_csv('train.csv')
+    artists = list(set(df['artist']))
+    counts = {artist: len(df[df.artist == artist]) for artist in artists if len(df[df.artist == artist]) >= 100}
+    for i, row in df.iterrows():
+        _, filename, artist, title, style, genere, date = row
+        if artist in counts:
+            src_path = os.path.join(old_path, filename)
+            dst_path = os.path.join(new_path, filename)
+            if os.path.exists(src_path):
+                if not os.path.exists(dst_path):
+                    shutil.move(src_path, dst_path)
 
 
 if __name__ == '__main__':
