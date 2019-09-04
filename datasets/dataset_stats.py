a324cd870fdf358327297db5fcd83639bb56ae50
itay <shamiritay@gmail.com>
2019-09-04 12:32:32 +0300

    use only artist with at least 100 images

--- datasets/dataset_stats.py
+++ datasets/dataset_stats.py
@@ -1,4 +1,6 @@
+import os
 import random
+import shutil
 from collections import Counter
 
 import pandas as pd
@@ -25,8 +27,36 @@ def split_train_test(train_per=0.9):
     df_test.to_csv('test.csv')
 
 
+def minimum_images():
+    df = pd.read_csv('train.csv')
+    # df = df[df.filename.str.startswith('4') | df.filename.str.startswith('3') |
+    #         df.filename.str.startswith('2') | df.filename.str.startswith('1')]
+
+    artists = list(set(df['artist']))
+    counts = {artist: len(df[df.artist == artist]) for artist in artists if len(df[df.artist == artist]) >= 100}
+    # print(counts.keys())
+    print(len(counts))
+    print(df.columns)
+    # for i, row in df.iterrows():
+    #     _, filename, artist, title, style, genere, date = row
+    #     if artist in counts:
+    #         src_path = os.path.join('train', filename)
+    #         dst_path = os.path.join('train_2', filename)
+    #         if os.path.exists(src_path):
+    #             if not os.path.exists(dst_path):
+    #                 shutil.copy(src_path, dst_path)
+
+    df_train = df[df['artist'].isin(list(counts.keys()))]
+    df_train.to_csv('new_train.csv')
+    print(len(df_train))
+
+    # counts = list(Counter(df['artist']).values())
+    # print((len(counts), np.mean(counts), np.std(counts), np.min(counts), np.max(counts)))
+
+
 if __name__ == '__main__':
     stats()
     # split_train_test()
     # stats('train.csv')
     # stats('test.csv')
+    # minimum_images()
